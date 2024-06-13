cc16b4d3e37c2f8580bfe60aa54460f5bd49028f
Learning-stud <jayahalpara123456@gmail.com>
2024-06-13 14:48:04 +0530

    modified:   Ecommerce/src/app/pages/admin/products /products.component.ts

--- Ecommerce/src/app/pages/admin/products/products.component.ts
+++ Ecommerce/src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrls: ['./products.component.css'] // Corrected 'styleUrl' to 'styleUrls'
 })
 export class ProductsComponent {
+  sidePanelVisible: boolean = true;
+  //  PRODUCT OBJECT FROM THE API
+  productObj: any = {
+    productId: 0,
+    productSku: "",
+    productName: "",
+    productPrice: 0,
+    productShortName: "",
+    productDescription: "",
+    createdDate: new Date(),
+    deliveryTimeSpan: "",
+    categoryId: "",
+    productImageUrl: ""
+  };
 
+
+  constructor(private productSrv: ProductService){
+
+  }
+  openSidePanel() {
+    this.sidePanelVisible = true;
+  }
+
+  closeSidePanel() {
+    this.sidePanelVisible = false;
+  }
 }
